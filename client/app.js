37615c09e7a3c7732c5a9fc0e247dc9e203c76b1
Teoman Soygul <tsoygul@gmail.com>
2015-11-25 11:59:59 +0100

    add build step

---
+++ client/app.js
@@ -0,0 +1,15 @@
+import * as ng from '../node_modules/angular2/es6/dev/angular2_sfx';
+
+var AppComponent = ng
+    .Component({
+        selector: 'my-app',
+        template: '<h1>My First Angular 2 App</h1>',
+    })
+    .Class({
+        constructor: function () {
+        }
+    });
+
+document.addEventListener('DOMContentLoaded', function () {
+    ng.bootstrap(AppComponent);
+});
